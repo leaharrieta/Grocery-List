4733a137f72729d09a66e5538e5211d3fd38dba2
Leah Arrieta <leaha3@umbc.edu>
2025-06-03 20:22:00 -0400

    A final function get_list was implemented to return either the entire list of a certain number of items according to the user. It will error out if the input is out of bounds.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # to run, in terminal run: uvicorn [filename]:[appname] --reload
     # uvicorn main:app --reload
 # press link
+# can also test at: http://127.0.0.1:8000/docs
 
 # note anytime changes are made to the file, the app refreshes
 # routes will be through the url when something is entered after the /
 
-# import fastapi
-from fastapi import FastAPI
+# import fastapi and error handler
+from fastapi import FastAPI, HTTPException
 # allows FastAPI to read JSON, convert types, validate data, and return errors
 from pydantic import BaseModel
 
@@ -18,7 +19,7 @@ items = []
 
 # define model for input
 class Item(BaseModel):
-    name : str
+    name : str  # required
 
 @app.get("/")   # and app decorator that defines a path for the http get method
 def root():     # when someone calls line above, this function is called
@@ -32,13 +33,25 @@ def root():     # when someone calls line above, this function is called
 @app.post("/items")
 def create_item(item : Item): # item is the input
     items.append(item.name)
-    return {"item added: " : item.name, "current items: " : items}
+    return {"item added: " : item.name, "current list: " : items}
 
 # retrives item at a specific index
 # to test run: curl http://127.0.0.1:8000/items/0
-@app.get("/items/{item_id}")    # {} b/c its a variable
+@app.get("/items/{item_id}", response_model = Item)    # {} b/c its a variable
 def get_item(item_id : int):
     if item_id < 0 or item_id >= len(items):
-        return {"Error: \"Invalid item_id\""}
+        raise HTTPException(status_code = 404, detail = f"Item {item_id} not found.")
+    
     item = items[item_id]
-    return item
+    return {"item at specified index: " : item, "current list: " : items}
+
+# return the entire list in left empty, else return the user-specified number of items starting fromm the top
+# to test the whole list returning, run: curl "http://127.0.0.1:8000/items"
+# to test returning a specific number: curl "http://127.0.0.1:8000/items?count=1"
+@app.get("/items", response_model = list[Item])
+def get_list(count : int = None):   # None tells FastAPI that count is optional and that the programmer will handle it if not provided
+    if count == None:   # return the whole list
+        return items
+    if count < 0 or count > len(items):
+        raise HTTPException(status_code=404, detail = f"Out of bounds.")
+    return items[0:count]
